814c2691e613c3bbc8974fb15164fdb98207ef37
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-08-12 22:02:43 -0300

    feat: result page

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,7 +1,20 @@
+import { useRouter } from "next/router";
+
+import styles from "../styles/Result.module.scss";
+
 export default function Results() {
+  const router = useRouter();
+
+  const total = +router.query.total;
+  const score = +router.query.score;
+  const percent = Math.round((score / total) * 100);
+
   return (
-    <div>
-      <h1>Results</h1>
+    <div className={styles.result}>
+      <h1>Resultado</h1>
+      <div>Total: {total}</div>
+      <div>Score: {score}</div>
+      <div>Porcentagem de acerto: {percent}%</div>
     </div>
   );
 }
